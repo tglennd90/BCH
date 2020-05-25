a6313a5066f7207701d87e4c35cf1bdff7f0cdcb
Tatum Daily <tgd9019@outlook.com>
2020-05-25 08:18:46 -0500

    User Login/Basic Authorization Added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI:"mongodb+srv://Tatum:WZ3hcrCSXW2MDxBh@cluster0-s6d5q.mongodb.net/test?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://Tatum:WZ3hcrCSXW2MDxBh@cluster0-s6d5q.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET:"983LEjubYLm7"
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../config/keys');
+const requireLogin = require('../middleware/requireLogin');
 
-router.get('/', (req,res) => {
-    res.send("hello from /routes/auth")
+// Protected User
+router.get('/protected', requireLogin, (req,res) => {
+    res.send("hello protected user")
 });
 
+// Sign-Up
 router.post('/signup', (req,res) => {
     const {name,email,password} = req.body;
 
@@ -20,22 +26,56 @@ router.post('/signup', (req,res) => {
                 return res.status(422).json({Error: "User already exists with that Email"})
             }
 
-            const user = new User({
-                name,
-                email,
-                password
-            })
+            bcrypt.hash(password,12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        name,
+                        email,
+                        password:hashedpassword
+                    })
 
-            user.save()
-                .then(user => {
-                    res.json({message: "Saved Successfully"})
-                })
-                .catch(err => {
-                    console.log(err)
+                    user.save()
+                        .then(user => {
+                            res.json({ message: "Saved Successfully" })
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
                 })
         })
         .catch(err => {
             console.log(err)
+        })
+});
+
+// Sign-In
+router.post('/signin', (req,res) => {
+    const {email,password} = req.body
+
+    if (!email || !password) {
+        return res.status(422).json({error: "Please provide email and password"})
+    }
+
+    User.findOne({email:email})
+        .then(savedUser => {
+            if(!savedUser) {
+                return res.status(422).json({error: "Invalid Email or Password"})
+            }
+            bcrypt.compare(password,savedUser.password)
+                .then(doMatch => {
+                    if(doMatch) {
+                        // res.json({message:"Successfully Logged In"})
+
+                        const token = jwt.sign({_id:savedUser._id},JWT_SECRET);
+
+                        res.json({token})
+                    } else {
+                        return res.status(422).json({error: "Invalid Email or Password"})
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         })
 });
 
